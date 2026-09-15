09a456df021d94fa113eb559a61ff21d6942472f
devendrk7 <devendrak9610@gmail.com>
2026-09-15 20:45:00 +0530

    Empty-data handling

--- log_analyzer/src/data_cleaner.py
+++ log_analyzer/src/data_cleaner.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
 def create_dataframe(parsed_logs):
+    if not parsed_logs:
+        return pd.DataFrame()
     df = pd.DataFrame(parsed_logs)
-    df["timestamp"] = pd.to_datetime(df["timestamp"],format="%d/%b/%Y:%H:%M:%S %z")
-    df = df.set_index("timestamp")
+    df["timestamp"] = pd.to_datetime(
+        df["timestamp"],
+        format="%d/%b/%Y:%H:%M:%S %z"
+    )
 
+    return df
+def clean_data(df):
+    if df.empty:
+        return df
+    df = df.dropna()
     return df
